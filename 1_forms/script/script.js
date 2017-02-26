a44eb0941403dd5959e7370a990f8e55949acd73
zarazpadne <joycephantasm@gmail.com>
2017-02-26 19:23:39 +0100

    forms rework

--- 1_forms/script/script.js
+++ 1_forms/script/script.js
@@ -7,3 +7,35 @@ function checkIfEmpty(element) {
 	if (element.value == '')
 		element.value = "Type your message...";
 }
+
+function checkIfDoneEmail(element) {
+	if (element.value != '') {
+		var x = document.getElementById("phone");
+		console.log(x.value);
+	}
+}
+
+function checkIfDoneEmail(element) {
+	var x = '';
+	if (element.value != '') {
+		x = document.getElementById("email").value;
+	}
+	return x;
+}
+
+function checkIfDonePhone(element) {
+	var x = '';
+	if (element.value != '') {
+		x = document.getElementById("phone").value;
+	}
+	return x;
+}
+
+function checkIfDone(element) {
+	var email = checkIfDoneEmail(element);
+	var phone = checkIfDonePhone(element);
+
+	if ((email != "") && (phone != "")) {
+
+	}
+}
